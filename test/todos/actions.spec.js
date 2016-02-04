ec8fea2cdc581ba6b44305b01dd2bba0247480b7
Bogdan Pohasii <bpo@ciklum.com>
2016-02-04 11:45:23 +0200

    [Test cases]

--- test/todos/actions.spec.js
+++ test/todos/actions.spec.js
@@ -25,6 +25,14 @@ describe('Actions:sync', () => {
         expect(actions.addTodo({todo})).to.deep.equal(expectedAction);
     });
 
+    it('clearAll: should create an action to delete all todos', () => {
+        const expectedAction = {
+            type: types.CLEAR_ALL
+        };
+
+        expect(actions.clearAll()).to.deep.equal(expectedAction);
+    });
+
     it('addTodo: should create an action to complete a todo', (done) => {
         const todoId = 200913;
 
