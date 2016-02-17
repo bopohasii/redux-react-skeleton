538d426cb72ed2fdf11ac363a636eb590f800048
Bogdan Pohasii <bpo@ciklum.com>
2016-02-17 15:30:47 +0200

    [Implement] SPA routing

--- develop/app.js
+++ develop/app.js
@@ -4,18 +4,22 @@ import '../node_modules/babel-core/polyfill';
 
 import React        from 'react';
 import ReactDOM     from 'react-dom';
-import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
+
+import { Router, useRouterHistory } from 'react-router';
+import { createHashHistory }        from 'history';
 
 import routes         from './config/appRoutes.jsx';
 import configureStore from './store/root.store';
+
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
 const initialState = {};
 const store = configureStore(initialState);
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router children={routes} history={browserHistory} />
+        <Router children={routes} history={appHistory} />
     </Provider>,
 
     document.getElementById('react-view')
