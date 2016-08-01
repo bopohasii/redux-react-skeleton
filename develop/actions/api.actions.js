98be13c696641ba4ca0f7d70db2ca014f1a7c183
Bogdan Pohasii <bpo@ciklum.com>
2016-08-01 11:00:19 +0300

    [Add] Api handlers structure

---
+++ develop/actions/api.actions.js
@@ -0,0 +1,55 @@
+import apiTypes from './types/api.types';
+
+
+// * Actions structure *
+//
+// - have a { type } property.
+// - have a { error } property.
+// - have a { payload } property.
+// - have a { meta } property.
+
+export default {
+    // save in the api Reducer
+    request(key, value = true) {
+        return {
+            type: apiTypes.SET_LOADING,
+            payload: { key, value },
+        };
+    },
+
+    success(type, payload) {
+        return (dispatch) => {
+            dispatch(this.request(type, false));
+
+            const action = payload
+                ? { type, payload }
+                : { type };
+
+            dispatch(action);
+        };
+    },
+
+    // save in the api Reducer
+    failure(key, error) {
+        return (dispatch) => {
+            dispatch(this.request(key, false));
+
+            const action = {
+                type: apiTypes.SET_ERROR,
+                error: { key, value: error },
+            };
+
+            dispatch(action);
+        };
+    },
+
+    // save in the api Reducer
+    paginator(key, paginator) {
+        const action = {
+            type: apiTypes.SET_PAGINATOR,
+            payload: { key, value: paginator },
+        };
+
+        return action;
+    },
+};
--- develop/reducers/pokeball.reducer.js
+++ develop/reducers/pokeball.reducer.js
@@ -2,59 +2,30 @@ import types from '../actions/types/pokeball.types';
 
 const DEFAULT_STATE = {
     pokemon: {
-        isLoading: false,
         isEmpty: false,
         entity: {},
-        error: null,
     },
     pokemons: {
-        isLoading: false,
         isEmpty: false,
         entities: [],
-        error: null,
     },
 };
 
 export default (state = DEFAULT_STATE, action) => {
     switch (action.type) {
-        case types.GET_POKEMONS_REQUEST: return getPokemonsRequest(state);
-        case types.GET_POKEMONS_SUCCESS: return getPokemonsSuccess(state, action);
-        case types.GET_POKEMONS_FAILURE: return getPokemonsFailure(state, action);
+        case types.GET_POKEMONS: return getPokemons(state, action);
 
         default:
             return state;
     }
 };
 
-function getPokemonsRequest(state) {
+function getPokemons(state, action) {
     return {
         ...state,
         pokemons: {
             ...state.pokemons,
-            isLoading: true,
-        },
-    };
-}
-
-
-function getPokemonsSuccess(state, action) {
-    return {
-        ...state,
-        pokemons: {
-            ...state.pokemons,
-            isLoading: false,
             entities: [...state.pokemons.entities, ...action.payload],
-        },
-    };
-}
-
-function getPokemonsFailure(state, action) {
-    return {
-        ...state,
-        pokemons: {
-            ...state.pokemons,
-            isLoading: false,
-            error: action.payload.error,
         },
     };
 }
