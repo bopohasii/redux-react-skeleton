e93d41e1d2f075a4ac590268a3448437b3d593d1
Bogdan Pohasii <bpo@ciklum.com>
2016-08-01 11:36:01 +0300

    [Add] browser history

--- develop/config/appHistory.js
+++ develop/config/appHistory.js
@@ -1,6 +1,15 @@
-import { useRouterHistory } from 'react-router';
-import { createHashHistory } from 'history';
 
-const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
+// Use browser history with server side settings (history-api-fallback)
+// Ex. For node express see 'express-history-api-fallback'
+// For webpack dev server run with '--history-api-fallback' flag
+import { browserHistory } from 'react-router';
+
+const appHistory = browserHistory;
+
+// Use hash history if you cannot configure server side
+// import { useRouterHistory } from 'react-router';
+// import { createHashHistory } from 'history';
+
+//const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
 export default appHistory;
