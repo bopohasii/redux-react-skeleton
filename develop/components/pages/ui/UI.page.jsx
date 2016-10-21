c417be6287e0c11c68ba31fbc30e47f46a79abf5
Vitaliy Lazarenko <vila@ciklum.com>
2016-10-21 13:07:55 +0300

    add UI page

---
+++ develop/components/pages/ui/UI.page.jsx
@@ -0,0 +1,46 @@
+import React, { Component, PropTypes } from 'react';
+
+import ModalContainer from '../../../containers/modal/Modal.container.jsx';
+
+import '../../../assets/stylesheets/index.sass';
+
+export default class LayoutPage extends Component {
+    renderContainerModal() {
+        const { windowWidth, isModalOpen, containerModalData, handleKeyDown } = this.props;
+
+        return !isModalOpen
+            ? null
+            : (
+            <ModalContainer
+                onKeyDown={handleKeyDown}
+                windowWidth={windowWidth}
+                containerModalData={containerModalData}
+            />
+        );
+    }
+
+    render() {
+        const { children } = this.props;
+        const headerStyle = { textAlign: 'center', paddingTop: '15px' };
+
+        return (
+            <div className="container-fluid">
+
+                <h2 className="hide" style={headerStyle}> {'<Pokémon GO|Noxwille />'} </h2>
+
+                {children}
+
+                {this.renderContainerModal()}
+
+            </div>
+        );
+    }
+}
+
+LayoutPage.propTypes = {
+    children: PropTypes.element,
+    windowWidth: PropTypes.number,
+    handleKeyDown: PropTypes.func,
+    containerModalData: PropTypes.object,
+    isModalOpen: PropTypes.bool,
+};
--- develop/config/appRoutes.jsx
+++ develop/config/appRoutes.jsx
@@ -6,6 +6,8 @@ import Layout from './../containers/layouts/Layout.container.jsx';
 import Pokemon from '../containers/pokemons/Pokemon.container.jsx';
 import Pokemons from '../containers/pokemons/Pokemons.container.jsx';
 
+import UIStyle from '../containers/ui/UI.container.jsx';
+
 export default (
     <Route path="/" component={App}>
         <Route component={Layout}>
@@ -15,6 +17,10 @@ export default (
                 <IndexRoute component={Pokemons} />
 
                 <Route path=":pokemonId" component={Pokemon} />
+            </Route>
+
+            <Route path="ui">
+                <IndexRoute component={UIStyle} />
             </Route>
 
         </Route>
