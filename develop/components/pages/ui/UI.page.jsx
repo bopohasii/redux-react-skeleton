fe366ef2608f49d08400538515bcbf9468814661
Vitaliy Lazarenko <vila@ciklum.com>
2016-10-24 13:19:56 +0300

    Module - Colors Palette

--- develop/components/pages/ui/UI.page.jsx
+++ develop/components/pages/ui/UI.page.jsx
@@ -35,9 +35,53 @@ export default class UIPage extends Component {
 
 
 
-                    <div className="ui__color"></div>
-                    <div className="ui__color-code-hex"></div>
-                    <div className="ui__color-code-rgba"></div>
+                    <div className="ui__color ui__color-text">
+                        <pre className="ui__color-name">
+                            $color-text
+                        </pre>
+                        <div className="ui__color-code-hex"></div>
+                        <div className="ui__color-code-rgba"></div>
+                    </div>
+
+                    <div className="ui__color ui__color-link">
+                        <pre className="ui__color-name">
+                            $color-link
+                        </pre>
+                        <div className="ui__color-code-hex"></div>
+                        <div className="ui__color-code-rgba"></div>
+                    </div>
+
+                    <div className="ui__color ui__color-red">
+                        <pre className="ui__color-name">
+                            $color-red
+                        </pre>
+                        <div className="ui__color-code-hex"></div>
+                        <div className="ui__color-code-rgba"></div>
+                    </div>
+
+                    <div className="ui__color ui__color-green">
+                        <pre className="ui__color-name">
+                            $color-green
+                        </pre>
+                        <div className="ui__color-code-hex"></div>
+                        <div className="ui__color-code-rgba"></div>
+                    </div>
+
+                    <div className="ui__color ui__color-blue">
+                        <pre className="ui__color-name">
+                            $color-blue
+                        </pre>
+                        <div className="ui__color-code-hex"></div>
+                        <div className="ui__color-code-rgba"></div>
+                    </div>
+
+                    <div className="ui__color ui__color-orange">
+                        <pre className="ui__color-name">
+                            $color-orange
+                        </pre>
+                        <div className="ui__color-code-hex"></div>
+                        <div className="ui__color-code-rgba"></div>
+                    </div>
 
 
                 </section>
