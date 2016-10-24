df117b1996b741a3b13fbc827d4f2fd070a0e5e1
oksanych <a.oksanych.90@gmail.com>
2016-10-24 15:01:50 +0300

    Added styles for titles, link and text.

--- develop/components/pages/ui/UI.page.jsx
+++ develop/components/pages/ui/UI.page.jsx
@@ -32,9 +32,7 @@ export default class UIPage extends Component {
                 <section className="ui__section">
 
                     <h2 className="ui__title">Colors Palette</h2>
-
-
-
+                    
                     <div className="ui__color"></div>
                     <div className="ui__color-code-hex"></div>
                     <div className="ui__color-code-rgba"></div>
@@ -42,11 +40,73 @@ export default class UIPage extends Component {
 
                 </section>
 
+                <section className="ui__section">
+                    <h2 className="ui__title">Typography</h2>
+                    
+                    <div className="ui__typography">
+                        <div className="ui__typography-item">
+                            <div className="ui__typography-preview">
+                                <h2 className="title title--large">Abcdefgh</h2>
+                            </div>
 
+                            <div className="ui__typography-name title">Title large</div>
+                            <div className="ui__typography-size">2.8 rem</div>
+                            <div className="ui__typography-weight">700/bold</div>
+                        </div>
 
+                        <div className="ui__typography-item">
+                            <div className="ui__typography-preview">
+                                <h2 className="title title--medium">Abcdefgh</h2>
+                            </div>
 
+                            <div className="ui__typography-name title">Title medium</div>
+                            <div className="ui__typography-size">2.4 rem</div>
+                            <div className="ui__typography-weight">700/bold</div>
+                        </div>
 
+                        <div className="ui__typography-item">
+                            <div className="ui__typography-preview">
+                                <h2 className="title title--small">Abcdefgh</h2>
+                            </div>
 
+                            <div className="ui__typography-name title">Title small</div>
+                            <div className="ui__typography-size">2 rem</div>
+                            <div className="ui__typography-weight">700/bold</div>
+                        </div>
+
+                        <div className="ui__typography-item">
+                            <div className="ui__typography-preview">
+                                <h2 className="title">Abcdefgh</h2>
+                            </div>
+
+                            <div className="ui__typography-name title">Title</div>
+                            <div className="ui__typography-size">1.6 rem</div>
+                            <div className="ui__typography-weight">700/bold</div>
+                        </div>
+                    </div>
+
+                    <div className="ui__typography">
+                        <div className="ui__typography-item">
+                            <div className="ui__typography-preview">
+                                <p className="text">Abcdefgh</p>
+                            </div>
+
+                            <div className="ui__typography-name title">Paragraph</div>
+                            <div className="ui__typography-size">1.6 rem</div>
+                            <div className="ui__typography-weight">500/normal</div>
+                        </div>
+
+                        <div className="ui__typography-item">
+                            <div className="ui__typography-preview">
+                                <a className="link" href="#">Abcdefgh</a>
+                            </div>
+
+                            <div className="ui__typography-name title">Link</div>
+                            <div className="ui__typography-size">1.6 rem</div>
+                            <div className="ui__typography-weight">500/normal</div>
+                        </div>
+                    </div>
+                </section>
 
                 {this.renderContainerModal()}
 
