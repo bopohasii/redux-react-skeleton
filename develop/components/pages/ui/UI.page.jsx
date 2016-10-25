1622cdc1afed9f4b569555beba28e207a53b3ff8
oksanych <a.oksanych.90@gmail.com>
2016-10-25 12:49:33 +0300

    Added base styles for navigation and breadcrumbs.

--- develop/components/pages/ui/UI.page.jsx
+++ develop/components/pages/ui/UI.page.jsx
@@ -133,6 +133,66 @@ export default class UIPage extends Component {
                     </div>
                 </section>
 
+                <div className="ui__section">
+                    <div className="ui__title">Navigation</div>
+
+                    <nav className="nav">
+                        <div className="nav__item active">
+                            <a href="#" className="nav__link">Menu-item 1</a>
+                        </div>
+                        <div className="nav__item">
+                            <a href="#" className="nav__link">Menu-item 2</a>
+                        </div>
+                        <div className="nav__item">
+                            <a href="#" className="nav__link">Menu-item 3</a>
+                        </div>
+                    </nav>
+
+
+                    <ul className="nav">
+                        <li className="nav__item active">
+                            <a href="#" className="nav__link">Menu-item 1</a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#" className="nav__link">Menu-item 2</a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#" className="nav__link">Menu-item 3</a>
+                        </li>
+
+                        <li className="nav__item nav__item--dropdown">
+                            <a href="#" className="nav__link">Menu-item dropdown</a>
+                            <ul className="nav__sub">
+                                <li className="nav__item">
+                                    <a href="#" className="nav__link">Menu-sub item 1</a>
+                                </li>
+                                <li className="nav__item">
+                                    <a href="#" className="nav__link">Menu-sub item 2</a>
+                                </li>
+                                <li className="nav__item">
+                                    <a href="#" className="nav__link">Menu-sub item 3</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="ui__section">
+                    <div className="ui__title">Breadcrumbs</div>
+
+                    <ul className="breadcrumbs">
+                        <li className="breadcrumbs__item">
+                            <a href="#" className="breadcrumbs__link">Home</a>
+                        </li>
+                        <li className="breadcrumbs__item">
+                            <a href="#" className="breadcrumbs__link">Library</a>
+                        </li>
+                        <li className="breadcrumbs__item active">
+                            <span className="breadcrumbs_text">Data</span>
+                        </li>
+                    </ul>
+                </div>
+
                 {this.renderContainerModal()}
 
             </div>
