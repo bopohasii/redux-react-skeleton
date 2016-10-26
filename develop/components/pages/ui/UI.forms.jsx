248fbaf629faf1dc580759eb4a3e2fb701cef99b
Vitaliy Lazarenko <vila@ciklum.com>
2016-10-26 17:51:21 +0300

    add btn

--- develop/components/pages/ui/UI.forms.jsx
+++ develop/components/pages/ui/UI.forms.jsx
@@ -9,23 +9,96 @@ class UIForms extends Component{
 
                 <h2 className="ui__title">Forms</h2>
 
-                <article className="ui__article">
+                <article className="ui__article ui-forms">
+
+                    <h4 className="ui__sub-title">Buttons</h4>
 
 
-                    <button className="">
-                        Default
+
+                    <div className="ui__btn-label">
+                        <div className="item">
+                            Default
+                        </div>
+                        <div className="item">
+                            Hover
+                        </div>
+                        <div className="item">
+                            Pressed
+                        </div>
+                    </div>
+
+                    <div className="ui__btn-type">
+                        <div className="item">
+                            <button className="button-default">
+                                Default
+                            </button>
+                        </div>
+                        <div className="item">
+                            <button className="button-default hover">
+                                Default
+                            </button>
+                        </div>
+                        <div className="item">
+                            <button className="button-default pressed">
+                                Default
+                            </button>
+                        </div>
+                    </div>
+                    <div className="ui__btn-type">
+                        <div className="item">
+                            <button className="button-primary">
+                                Primary
+                            </button>
+                        </div>
+                        <div className="item">
+                            <button className="button-primary hover">
+                                Primary
+                            </button>
+                        </div>
+                        <div className="item">
+                            <button className="button-primary pressed">
+                                Primary
+                            </button>
+                        </div>
+                    </div>
+                    <div className="ui__btn-type">
+                        <div className="item">
+                            <button className="button-success">
+                                Success
+                            </button>
+                        </div>
+                        <div className="item">
+                            <button className="button-success hover">
+                                Success
+                            </button>
+                        </div>
+                        <div className="item">
+                            <button className="button-success pressed">
+                                Success
+                            </button>
+                        </div>
+                    </div>
+                    <div className="ui__btn-type">
+                        <div className="item">
+                            <button className="button-danger">
+                                Danger
+                            </button>
+                        </div>
+                        <div className="item">
+                            <button className="button-danger hover">
+                                Danger
+                            </button>
+                        </div>
+                        <div className="item">
+                            <button className="button-danger pressed">
+                                Danger
+                            </button>
+                        </div>
+                    </div>
+
+                    <button className="button-default">
+                        Cancel
                     </button>
-
-
-                    <button className="button-primary">
-                        Primary
-                    </button>
-
-
-
-
-
-
 
                 </article>
 
