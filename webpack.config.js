c7ff841715000938fef5823439f352ecf83ad5fd
Bogdan Pohasii <bogdanpogasiy@gmail.com>
2016-10-28 11:32:47 +0300

    [Add] optimization for svg

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const SpritePlugin = require('webpack-spritesmith');
 const path = require('path');
 
 module.exports = {
@@ -17,7 +18,8 @@ module.exports = {
             { test: /\.gif$/, loader: 'url?limit=16000&mimetype=image/gif&name=[name].[ext]?[hash]' },
             { test: /\.jpg$/, loader: 'url?limit=100000&mimetype=image/jpg&name=[name].[ext]?[hash]' },
             { test: /\.png$/, loader: 'url?limit=50000&mimetype=image/png&name=[name].[ext]?[hash]' },
-            { test: /\.svg/, loader: 'url?limit=50000&mimetype=image/svg+xml&name=[name].[ext]?[hash]' },
+
+            { test: /\.svg/, loader: 'url?limit=50000&mimetype=image/svg+xml&name=[name].[ext]?[hash]!svgo' },
 
             // {
             //     test: /.*\.(gif|png|jpe?g|svg)$/i,
@@ -52,6 +54,16 @@ module.exports = {
             template: 'develop/index.html',
             inject: 'body',
         }),
+        // new SpritePlugin({
+        //     src: {
+        //         cwd: path.resolve(__dirname, 'develop/assets/svg'),
+        //         glob: '*.svg',
+        //     },
+        //     target: {
+        //         image: path.resolve(__dirname, 'develop/assets/svg-sprite/sprite.svg'),
+        //         css: path.resolve(__dirname, 'develop/assets/svg-sprite/sprite.sass'),
+        //     },
+        // }),
     ],
 };
 
